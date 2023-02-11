f1a94ca158182f029d2d736a68b8183f9f7f6e7c
RafaFull <dejerafa@gmail.com>
2023-02-11 15:45:00 -0300

    Adição de um dicionãrio

---
+++ meu-primeiro-projeto-java-dio/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    
+public static void main (String [] args){
+    System.out.print("Olá pessoal!, sejam todos bem vindos");
+}
+
+}
+
+//Vamos a nossa primeira explicação. 
+//Quando o "final" fica no começo de uma string "final String BR = "Brasil"; significa que o valor daquela string não poderá
+//mais ser alterado.
+//Agora quando a string "int ano = 2021; ano = 2022" ela poderá ser alterada futuramente.
+
+//Uppercase = strings imutáveis //double PI = 3.14; int ESTADOS_BRASILEIROS = 27; int ANO_2000 = 2000;
+
