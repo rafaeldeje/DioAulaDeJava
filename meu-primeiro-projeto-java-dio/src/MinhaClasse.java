fc168500fde48168d9e55c3d6b060355232c3c18
RafaFull <dejerafa@gmail.com>
2023-02-11 15:50:49 -0300

    Primeiro código

--- meu-primeiro-projeto-java-dio/src/MinhaClasse.java
+++ meu-primeiro-projeto-java-dio/src/MinhaClasse.java
@@ -6,10 +6,4 @@ public static void main (String [] args){
 
 }
 
-//Vamos a nossa primeira explicação. 
-//Quando o "final" fica no começo de uma string "final String BR = "Brasil"; significa que o valor daquela string não poderá
-//mais ser alterado.
-//Agora quando a string "int ano = 2021; ano = 2022" ela poderá ser alterada futuramente.
-
-//Uppercase = strings imutáveis //double PI = 3.14; int ESTADOS_BRASILEIROS = 27; int ANO_2000 = 2000;
-
+//Primeiro código, olá mundo!
